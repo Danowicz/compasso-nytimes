fe0a56ecb76e30eab28abfd39d61e13132f10850
Alexander Danowicz <contact@danowicz.dev>
2021-03-13 22:50:00 -0300

    feat: Setting icons OnInit

--- src/app/components/switch/switch.component.ts
+++ src/app/components/switch/switch.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IconName, IconPrefix } from '@fortawesome/fontawesome-svg-core';
@@ -7,26 +8,44 @@ import { IconName, IconPrefix } from '@fortawesome/fontawesome-svg-core';
     templateUrl: './switch.component.html',
     styleUrls: ['./switch.component.scss'],
 })
-export class SwitchComponent {
+export class SwitchComponent implements OnInit {
 
     public selectedFilter: string;
     public icon: [IconPrefix, IconName] = ['fas', 'exchange-alt'];
 
-    constructor(private router: Router) {}
+    constructor(private router: Router, private location: Location) {}
 
-    switchFilter(): void {
+    ngOnInit(): void {
+        this.selectedFilter = this.location.path().replace('/','');
+        this.iconSetter();
+    }
+
+    public switchFilter(): void {
         switch (this.selectedFilter) {
             case 'science':
                 this.selectedFilter = 'tech';
-                this.router.navigate(['technology']);
-                this.icon = ['fas', 'rocket'];
+                this.router.navigate(['tech']);
                 break;
             case 'tech':
                 this.selectedFilter = 'science';
                 this.router.navigate(['science']);
+                break;
+            default:
+                this.selectedFilter = 'science';
+                this.router.navigate(['science']);
+        }
+        this.iconSetter();
+    }
+
+    private iconSetter(): void {
+        switch (this.selectedFilter) {
+            case 'science':
                 this.icon = ['fas', 'atom'];
                 break;
-            default: this.selectedFilter = '';
+            case 'tech':
+                this.icon = ['fas', 'rocket'];
+                break;
+            default: this.icon = ['fas', 'exchange-alt'];
         }
     }
 }
