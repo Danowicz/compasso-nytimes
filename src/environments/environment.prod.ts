7cf1d05357c7cf996e88096e317e0800366e34c5
Alexander Danowicz <contact@danowicz.dev>
2021-03-14 13:28:17 -0300

    feat: Adding NYT API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import { NYT_API_KEY } from '../../env';
 
 export const environment = {
   production: true,
+  API_URL: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
   API_KEY: NYT_API_KEY
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ import { NYT_API_KEY } from '../../env'
 
 export const environment = {
   production: false,
+  API_URL: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
   API_KEY: NYT_API_KEY
 };
