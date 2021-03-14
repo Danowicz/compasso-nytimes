386e60886d56af19db88c2748ba271dd9d375c9e
Alexander Danowicz <contact@danowicz.dev>
2021-03-14 13:34:31 -0300

    feat: Using route as parameters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ContentWrapperComponent } from './components/content-wrapper/content-wrapper.component';
 
 const routes: Routes = [
     { path: '', component: AppComponent} ,
-    { path: 'science', component: AppComponent },
-    { path: 'tech', component: AppComponent }
+    { path: ':type', component: ContentWrapperComponent }
 ];
 
 @NgModule({
