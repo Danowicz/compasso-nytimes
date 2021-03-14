4eabc6877fcdc6d4b5eee2044309f35999b32569
Alexander Danowicz <contact@danowicz.dev>
2021-03-13 21:28:55 -0300

    feat: Adding routes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('compasso-nytimes');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('compasso-nytimes app is running!');
-  });
 });
