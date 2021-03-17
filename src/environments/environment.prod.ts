04c1d4c9c166756a7ae3a26aac06beef19179379
Alexander Danowicz <contact@danowicz.dev>
2021-03-17 15:32:48 -0300

    fix: Removing API_KEY

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
-import { NYT_API_KEY } from '../../env';
-
 export const environment = {
   production: true,
   API_URL: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
-  API_KEY: NYT_API_KEY
+  API_KEY: ''
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
-import { NYT_API_KEY } from '../../env'
-
 export const environment = {
   production: false,
   API_URL: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
-  API_KEY: NYT_API_KEY
+  API_KEY: ''
 };
